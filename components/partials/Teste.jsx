095fc765563953c028af5fca29c581e01a020d6a
Paulo <paulodigitalart@gmail.com>
2021-01-31 14:45:55 -0300

    update-01

---
+++ components/partials/Teste.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Footer = styled.div`
+    background-color: 'red';
+    background-position: bottom;
+    width: auto;
+    position: fixed;
+    height: 20%;
+`;
+
+
+export default function Teste(){
+    return(
+
+        <Footer>
+            sad
+        </Footer>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,56 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import "tailwindcss/tailwind.css";
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secundary};
 `
 
+const BackgroundImage = styled.div`
+  
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+  background-color: red;
+  width: auto;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 94%;
+  &:hover {
+    opacity: 98%;
+    transition: 1000ms;
+    transition-delay: 0,25s;
+    -webkit-transform: translateZ(1);
+  }
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screean and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <div className="bg-black dark border-black border-">
+    <BackgroundImage>
+        <QuizContainer>
+          <Title>
+            Quiz
+          </Title>
+        </QuizContainer>
+    </BackgroundImage>
+  
+    </div>
+    
+  );
 }
